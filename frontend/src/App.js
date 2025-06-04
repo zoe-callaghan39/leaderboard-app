2eadd83493b919cc628a341ed3499a3a2af7e560
zca01 <zoe.callaghan@skybettingandgaming.com>
2025-06-04 03:23:38 +0100

    Replacing  usage of BrowserRouter with HashRouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CurrentLeaderboard from "./pages/CurrentLeaderboard";
 import AllTimeLeaderboard from "./pages/AllTimeLeaderboard";
